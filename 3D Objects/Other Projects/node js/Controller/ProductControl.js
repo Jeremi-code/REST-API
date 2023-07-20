dc2936a2ca45bed2be4501fd0a4b2d11d787d0df
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-07-20 05:18:46 -0700

    add the controller file

---
+++ 3D Objects/Other Projects/node js/Controller/ProductControl.js
@@ -0,0 +1,99 @@
+const Product = require('../Model/productModel')
+const products=require('../Data/products.json')
+const{getPostData}=require('../util')
+
+async function getProducts(req,res){
+    try {
+        const products= await Product.findAll()
+        res.writeHead(200,{'Content-Type':'application/json'})
+        res.end(JSON.stringify(products))
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+async function getProduct(req,res,id){
+    try {
+        const products= await Product.findByID(id)
+        if (!products){
+            res.writeHead(200,{'Content-Type':'application/json'})
+            res.end(JSON.stringify({Message:'Message not found '}))
+        }
+        else{
+            res.writeHead(200,{'Content-Type':'application/json'})
+            res.end(JSON.stringify({products}))
+
+        }
+    } 
+        catch (error) {
+
+
+        }
+        console.log(error)
+        
+    }
+    async function createProduct(req,res){
+        try{
+            const body=await getPostData(req)
+            const{title,description,price}=JSON.parse(body)
+            const product={
+                title,
+                description,
+                price
+            }
+            const newProduct=await Product.create(product)
+            res.writeHead(201,{'Content-Type':'application/json'})
+            res.end(JSON.stringify({Message:'message not found'}))
+     
+     
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+    async function updateProduct(req,res,id){
+        try{
+            const product=await Product.findByID(id)
+            if(!product){
+                res.writeHead(404,{'Content-Type':'application/json'})
+                res.end(JSON.stringify({Message:'Message not found'}))
+            }
+            else{
+
+                const body=await getPostData(req)
+                const {title,description,price}=JSON.parse(body)
+                const productData={
+                    title:title || product.title,
+                    description:description || product.description,
+                    price:price || product.price
+                }
+                const updProduct=await update(id,productData)
+                res.writeHead(200,{'Content-Type':'application/json'})
+                res.end(JSON.stringify({Message:`${updProduct}`}))
+
+            }
+        }
+    catch(error){
+        console.log(error)
+    }
+}
+async function deleteProduct(req,res,id){
+    try{
+    const product= Product.findByID(id)
+    if(!product){
+        res.writeHead(200,{'Content-Type':'application/json'})
+        res.end(JSON.stringify({Message:'Message not found '}))
+    }
+    else{
+        
+
+        await Product.deleteProd(id)
+        res.writeHead(200,{'Content-Type':'application/json'})
+        res.end(JSON.stringify({Message:`product ${product} has been removed `}))
+    }
+}
+    catch(error){
+
+    }
+}
+module.exports={getProducts,getProduct,createProduct,updateProduct}
